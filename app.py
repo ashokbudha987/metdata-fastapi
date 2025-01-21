d4eda8177bab17be3b01de50dc3266de4fc6a34d
ashokbudha987 <ashok.budha@yco.com.np>
2025-01-21 14:06:14 +0545

    database ddl and sample rows feature added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from helpers import combine_metadata, extract_metadata_user_query
+from itlm_db_connector.factory import DatabaseFactory
 app = FastAPI()
 
 class QueryRequest(BaseModel):
@@ -8,6 +9,17 @@ class QueryRequest(BaseModel):
 
 class MetadataResponse(BaseModel):
     extracted_metadata: str
+
+class SampleDataRequest(BaseModel):
+    database_type: str
+    db_config: dict
+    table_name: str
+
+class SampleDataResponse(BaseModel):
+    table_ddl: str
+    sample_rows_query: str
+
+
 
 final_metrics, final_attributes, final_columns, final_functions = combine_metadata()
 
@@ -19,3 +31,18 @@ async def extract_metadata(request: QueryRequest):
     except Exception as e:
         raise
         # raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/get_metadata", response_model=SampleDataResponse)
+async def get_metadata(request: SampleDataRequest):
+    try:
+        db_config = request.db_config
+        connector = DatabaseFactory.get_connector(
+            request.database_type
+        )(**db_config)
+        table_ddl = connector.get_ddl(table_name=request.table_name)
+        sample_rows_query = connector.get_sample_rows_query(table_name=request.table_name)
+        return {"table_ddl": table_ddl, "sample_rows_query": sample_rows_query}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
